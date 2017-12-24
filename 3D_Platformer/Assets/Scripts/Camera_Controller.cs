1e29f9306d6950e58c8dcc2d09f12f8db7068ff4
jkatan <jkatan@users.noreply.github.com>
2017-12-24 02:50:29 -0300

    Character Controller versión inicial

---
+++ 3D_Platformer/Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+
+	public Transform target;
+	public float lookSmooth = 0.09f;
+	public Vector3 offSetFromTarget = new Vector3(0, 2 ,-4);
+
+	private Vector3 destination = Vector3.zero;
+	Character_Controller characterController;
+	float rotateVel = 0;
+
+	void Start() {
+
+		setCameraTarget (target);
+	}
+
+	public void setCameraTarget(Transform target) {
+
+		this.target = target;
+
+		if (this.target != null) {
+			if (this.target.GetComponent<Character_Controller> ())
+				characterController = this.target.GetComponent<Character_Controller> ();
+		} else
+			Debug.LogError ("La cámara necesita un target.");
+	}
+
+	void LateUpdate() {
+		MoveToTarget ();
+		LookAtTarget ();
+	}
+
+	void MoveToTarget() {
+		destination = characterController.CurrentRotation * offSetFromTarget;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	void LookAtTarget() {
+		float eulerYAngle = Mathf.SmoothDampAngle (transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+		transform.rotation = Quaternion.Euler (transform.eulerAngles.x, eulerYAngle, 0);
+	}
+}
---
+++ 3D_Platformer/Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour {
+
+	public float inputDelay = 0.1f;		//Delay para que el personaje se mueva un poco después de presionar la tecla
+	public float forwardVelocity = 12;
+	public float rotateVelocity = 100;
+
+	private Quaternion currentRotation;
+	private Rigidbody rigidBody;
+	private float forwardInput;
+	private float turnInput;
+
+	public Quaternion CurrentRotation {
+		get { return currentRotation; }
+	}
+
+	void Start() {
+		currentRotation = transform.rotation;
+		if (GetComponent<Rigidbody> ())
+			rigidBody = GetComponent<Rigidbody> ();
+		else
+			Debug.LogError ("El personaje necesita un rigidbody.");
+
+		forwardInput = 0;
+		turnInput = 0;
+	}
+
+	void GetInput() {
+		forwardInput = Input.GetAxis ("Vertical");
+		turnInput = Input.GetAxis ("Horizontal");
+	}
+
+	void Update() {
+		GetInput ();
+		Run ();
+		Turn ();
+	}
+
+	void Run() {
+		if (Mathf.Abs (forwardInput) > inputDelay) {
+			rigidBody.velocity = transform.forward * forwardInput * forwardVelocity;
+		} else
+			rigidBody.velocity = Vector3.zero;
+	}
+
+	void Turn() {
+		if (Mathf.Abs (turnInput) > inputDelay) {
+			currentRotation *= Quaternion.AngleAxis (rotateVelocity * turnInput * Time.deltaTime, Vector3.up); 
+		}
+
+		transform.rotation = currentRotation;
+	}
+}
