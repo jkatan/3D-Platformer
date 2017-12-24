40be82e0345666ad31b5b6a86b51a61cb3c6b60f
jkatan <jkatan@users.noreply.github.com>
2017-12-24 20:35:17 -0300

    Character Controller script, falta completar el doble salto

--- 3D_Platformer/Assets/Scripts/Character_Controller.cs
+++ 3D_Platformer/Assets/Scripts/Character_Controller.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class Character_Controller : MonoBehaviour {
 
-	public float inputDelay = 0.1f;		//Delay para que el personaje se mueva un poco después de presionar la tecla
-	public float forwardVelocity = 12;
-	public float rotateVelocity = 100;
+	[System.Serializable]
+	public class MoveSettings {
+		public float forwardVelocity = 8;
+		public float rotateVelocity = 100;
+		public float jumpVelocity = 8;
+		public float distanceToGround = 0.5f;
+		public LayerMask ground;
+	}
 
-	private Quaternion currentRotation;
-	private Rigidbody rigidBody;
+	[System.Serializable]
+	public class PhysicsSettings {
+		public float downAcceleration = 0.75f;
+	}
+
+	[System.Serializable]
+	public class InputSettings {
+		public float inputDelay = 0.1f;		//Delay para que el personaje se mueva un poco después de presionar la tecla
+		public string FORWARD_AXIS = "Vertical";
+		public string TURN_AXIS = "Horizontal";
+		public string JUMP_AXIS = "Jump";
+	}
+
+	public MoveSettings moveSettings = new MoveSettings();
+	public PhysicsSettings physicsSettings = new PhysicsSettings();
+	public InputSettings inputSettings = new InputSettings();
+
+	private Vector3 velocity = Vector3.zero;
 	private float forwardInput;
 	private float turnInput;
+	private float jumpInput;
+	private Quaternion currentRotation;
+	private Rigidbody rigidBody;
 
 	public Quaternion CurrentRotation {
 		get { return currentRotation; }
+	}
+
+	bool Grounded() {
+		return Physics.Raycast (transform.position, Vector3.down, moveSettings.distanceToGround, moveSettings.ground);
 	}
 
 	void Start() {
@@ -26,31 +54,54 @@ public class Character_Controller : MonoBehaviour {
 
 		forwardInput = 0;
 		turnInput = 0;
+		jumpInput = 0;
 	}
 
 	void GetInput() {
-		forwardInput = Input.GetAxis ("Vertical");
-		turnInput = Input.GetAxis ("Horizontal");
+		forwardInput = Input.GetAxis (inputSettings.FORWARD_AXIS);
+		turnInput = Input.GetAxis (inputSettings.TURN_AXIS);
+		jumpInput = Input.GetAxisRaw (inputSettings.JUMP_AXIS);
 	}
 
 	void Update() {
 		GetInput ();
-		Run ();
 		Turn ();
 	}
 
+	void FixedUpdate() {
+		Run ();
+		Jump ();
+		rigidBody.velocity = transform.TransformDirection(velocity);
+	}
+
 	void Run() {
-		if (Mathf.Abs (forwardInput) > inputDelay) {
-			rigidBody.velocity = transform.forward * forwardInput * forwardVelocity;
+		if (Mathf.Abs (forwardInput) > inputSettings.inputDelay) {
+			velocity.z = forwardInput * moveSettings.forwardVelocity;
 		} else
-			rigidBody.velocity = Vector3.zero;
+			velocity.z = 0;
 	}
 
 	void Turn() {
-		if (Mathf.Abs (turnInput) > inputDelay) {
-			currentRotation *= Quaternion.AngleAxis (rotateVelocity * turnInput * Time.deltaTime, Vector3.up); 
+		if (Mathf.Abs (turnInput) > inputSettings.inputDelay) {
+			currentRotation *= Quaternion.AngleAxis (moveSettings.rotateVelocity * turnInput * Time.deltaTime, Vector3.up); 
 		}
 
 		transform.rotation = currentRotation;
+	}
+
+	void Jump() {
+
+		if ((jumpInput > 0 && Grounded ()) || (jumpInput > 0 && !Grounded ())) {
+			
+			velocity.y = moveSettings.jumpVelocity;
+
+		} else if (jumpInput == 0 && Grounded ()) {
+			
+			velocity.y = 0;
+
+		} else {
+
+			velocity.y -= physicsSettings.downAcceleration;
+		}
 	}
 }
